3ad05226178042d4006ae2e0f6f50e363b60f2e4
akitora <hayashi20030703@gmail.com>
2024-12-27 18:12:34 +0900

    Initial commit

---
+++ multi_counter_app/View/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  multi_counter_app
+//
+//  Created by 林 明虎 on 2024/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ multi_counter_appUITests/multi_counter_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  multi_counter_appUITestsLaunchTests.swift
+//  multi_counter_appUITests
+//
+//  Created by 林 明虎 on 2024/12/27.
+//
+
+import XCTest
+
+final class multi_counter_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
