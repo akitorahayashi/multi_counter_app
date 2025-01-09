c3977838adce1d9b241403bf56a1f71da896c4dc
akitora <hayashi20030703@gmail.com>
2025-01-09 11:22:09 +0900

    -ボタンが正しく機 能することをテストできるようになった

--- multi_counter_appUITests/MultiCounterTableTests/CounterCellTests.swift
+++ multi_counter_appUITests/MultiCounterTableTests/CounterCellTests.swift
@@ -34,14 +34,36 @@ class CounterCellTests: XCTestCase {
         incrementButton.tap()
         let incrementedValue = Int(countLabel.label)
         XCTAssertNotNil(currentValue)
-        XCTAssertEqual(currentValue! + 1, incrementedValue, "+ボタンを押してもcountが1増えていない")
+        XCTAssertEqual(incrementedValue, currentValue! + 1, "+ボタンを押してもcountが1増えていない")
         
         // +ボタンを複数回タップして値が正しく増えることを確認
         for _ in 0..<5 {
             incrementButton.tap()
         }
         let furtherIncrementedValue = Int(countLabel.label)
-        XCTAssertEqual(currentValue! + 6, furtherIncrementedValue, "複数回タップしたときにcountが正しく増加できていない")
+        XCTAssertEqual(furtherIncrementedValue, currentValue! + 6, "複数回タップしたときにcountが正しく増加できていない")
+    }
+    
+    func testDecrementButtonWorks() throws {
+        let tableView = app.tables["CounterTableView"]
+        // セルの中のボタンを取得
+        let defaultCell = tableView.cells.element(boundBy: 0)
+        let decrementButton = defaultCell.buttons["-"]
+        // セルに紐づいている現在地を取得
+        let countLabel = defaultCell.staticTexts.matching(identifier: "CounterOfThisCell").element
+        let currentValue = Int(countLabel.label)
         
+        // -ボタンをタップして値が減ることを確認
+        decrementButton.tap()
+        let decrementedValue = Int(countLabel.label)
+        XCTAssertNotNil(currentValue)
+        XCTAssertEqual(decrementedValue, currentValue! - 1, "-ボタンを押してもcountが1減っていない")
+        
+        // -ボタンを複数回タップして値が正しく減ることを確認
+        for _ in 0..<5 {
+            decrementButton.tap()
+        }
+        let furtherDecrementedValue = Int(countLabel.label)
+        XCTAssertEqual(furtherDecrementedValue, currentValue! - 6, "複数回タップしたときにcountが正しく減少できていない")
     }
 }
